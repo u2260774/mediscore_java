49348700fbea3ad1cdf317a471acf8f332bf5da4
u2260774 <126501906+u2260774@users.noreply.github.com>
2024-02-21 23:55:48 +0000

    Update Main.java

--- MediScore_Java_Classes/src/Main.java
+++ MediScore_Java_Classes/src/Main.java
@@ -1,26 +1,20 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         try {
             Patient typical = new Patient();
-            typical.setAll(0, 0, 15, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(typical.getScores()));
-            System.out.println(typical);
+            System.out.println(typical.calculateMediScore(0, 0, 15, 95, 37.1f, 6.4f, 0));
+            System.out.println(typical.isFlagged());
             Patient moderate = new Patient();
-            moderate.setAll(2, 0, 17, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(moderate.getScores()));
-            System.out.println(moderate);
+            System.out.println(moderate.calculateMediScore(2, 0, 17, 95, 37.1f, 6.4f, 0));
+            System.out.println(moderate.isFlagged());
             Patient severe = new Patient();
-            severe.setAll(2, 1, 23, 88, 38.5f, 6.4f, 0);
-            System.out.println(Arrays.toString(severe.getScores()));
-            System.out.println(severe);
+            System.out.println(severe.calculateMediScore(2, 1, 23, 88, 38.5f, 6.4f, 0));
+            System.out.println(severe.isFlagged());
             Patient gettingWorse = new Patient();
-            gettingWorse.setAll(0, 0, 15, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(gettingWorse.getScores()));
-            System.out.println(gettingWorse);
-            gettingWorse.setAll(2, 0, 15, 95, 37.1f, 9.4f, 3);
-            System.out.println(Arrays.toString(gettingWorse.getScores()));
+            System.out.println(gettingWorse.calculateMediScore(0, 0, 15, 95, 37.1f, 6.4f, 0));
+            System.out.println(gettingWorse.isFlagged());
+            System.out.println(gettingWorse.calculateMediScore(2, 0, 15, 95, 37.1f, 9.4f, 3));
+            System.out.println(gettingWorse.isFlagged());
             System.out.println(gettingWorse);
         }
         catch (Exception e){
