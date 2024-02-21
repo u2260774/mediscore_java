222ae8d81846e5a1171fcaacae09852cd75daa01
u2260774 <126501906+u2260774@users.noreply.github.com>
2024-02-21 02:51:27 +0000

    Update Main.java

--- MediScore_Java_Classes/src/Main.java
+++ MediScore_Java_Classes/src/Main.java
@@ -1,25 +1,19 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         try {
             Patient typical = new Patient();
-            typical.setAll(Patient.respTypeValue.AIR, Patient.consciousnessTypeValue.ALERT, 15, 95, 37.1f, 6.4f, 3);
-            System.out.println(Arrays.toString(MediScore.calculate(typical)));
-
+            typical.setAll(Patient.respTypeValue.AIR, Patient.consciousnessTypeValue.ALERT, 15, 95, 37.1f, 6.4f, 0);
+            System.out.println(typical);
             Patient moderate = new Patient();
             moderate.setAll(Patient.respTypeValue.OXYGEN, Patient.consciousnessTypeValue.ALERT, 17, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(MediScore.calculate(moderate)));
-
+            System.out.println(moderate);
             Patient severe = new Patient();
             severe.setAll(Patient.respTypeValue.OXYGEN, Patient.consciousnessTypeValue.CVPU, 23, 88, 38.5f, 6.4f, 0);
-            System.out.println(Arrays.toString(MediScore.calculate(severe)));
-
+            System.out.println(severe);
             Patient gettingWorse = new Patient();
             gettingWorse.setAll(Patient.respTypeValue.AIR, Patient.consciousnessTypeValue.ALERT, 15, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(MediScore.calculate(gettingWorse)));
-            gettingWorse.setAll(Patient.respTypeValue.OXYGEN, Patient.consciousnessTypeValue.ALERT, 15, 95, 37.1f, 6.4f, 0);
-            System.out.println(Arrays.toString(MediScore.calculate(gettingWorse)));
+            System.out.println(gettingWorse);
+            gettingWorse.setAll(Patient.respTypeValue.OXYGEN, Patient.consciousnessTypeValue.ALERT, 15, 95, 37.1f, 9.4f, 3);
             System.out.println(gettingWorse);
         }
         catch (Exception e){
