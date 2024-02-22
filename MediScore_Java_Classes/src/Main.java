e942155f260a36a328a181624d9a337e2d93af64
u2260774 <126501906+u2260774@users.noreply.github.com>
2024-02-22 01:17:56 +0000

    Update Main.java

--- MediScore_Java_Classes/src/Main.java
+++ MediScore_Java_Classes/src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         try {
             Patient typical = new Patient();
-            System.out.println(typical.calculateMediScore(0, 0, 15, 95, 37.1f, 6.4f, 0));
+            System.out.println(typical.calculateMediScore(Patient.respTypeValue.AIR.value, Patient.consciousnessTypeValue.ALERT.value, 15, 95, 37.1f, 6.4f, 0));
             System.out.println(typical.isFlagged());
             Patient moderate = new Patient();
-            System.out.println(moderate.calculateMediScore(2, 0, 17, 95, 37.1f, 6.4f, 0));
+            System.out.println(moderate.calculateMediScore(Patient.respTypeValue.OXYGEN.value, Patient.consciousnessTypeValue.ALERT.value, 17, 95, 37.1f, 6.4f, 0));
             System.out.println(moderate.isFlagged());
             Patient severe = new Patient();
-            System.out.println(severe.calculateMediScore(2, 1, 23, 88, 38.5f, 6.4f, 0));
+            System.out.println(severe.calculateMediScore(Patient.respTypeValue.OXYGEN.value, 1, 23, 88, 38.5f, 6.4f, 0));
             System.out.println(severe.isFlagged());
             Patient gettingWorse = new Patient();
-            System.out.println(gettingWorse.calculateMediScore(0, 0, 15, 95, 37.1f, 6.4f, 0));
+            System.out.println(gettingWorse.calculateMediScore(Patient.respTypeValue.AIR.value, Patient.consciousnessTypeValue.ALERT.value, 15, 95, 37.1f, 6.4f, 0));
             System.out.println(gettingWorse.isFlagged());
-            System.out.println(gettingWorse.calculateMediScore(2, 0, 15, 95, 37.1f, 9.4f, 3));
+            System.out.println(gettingWorse.calculateMediScore(Patient.respTypeValue.OXYGEN.value, Patient.consciousnessTypeValue.ALERT.value, 15, 95, 37.1f, 9.4f, 3));
             System.out.println(gettingWorse.isFlagged());
             System.out.println(gettingWorse);
         }
